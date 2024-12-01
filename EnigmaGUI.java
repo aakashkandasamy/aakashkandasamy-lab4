17702585d9ae6fe8e70e8cdf5ac9147eaf941f8c
aakashkandasamy <aakashkandasamy@gmail.com>
2024-12-01 13:36:34 -0500

    updating gui file

--- EnigmaGUI.java
+++ EnigmaGUI.java
@@ -2,7 +2,6 @@ import javax.swing.SwingUtilities;
 
 public class EnigmaGUI {
     public static void main(String[] args) {
-        // create and show the frame
         EnigmaFrame myFrame = new EnigmaFrame();
         myFrame.setVisible(true);
     }
